799047b0d773495b0330df031e16a668090ebafc
prathamkumarjha <prathamjha123dcs@gmail.com>
2023-12-13 12:42:10 +0530

    new changes

--- components/modals/store-modals.tsx
+++ components/modals/store-modals.tsx
@@ -1,10 +1,25 @@
 "use client"
 
+import * as z from "zod"; 
+
 import {useStoreModal} from "@/hooks/use-store-modal"
 import { Modal } from "@/components/ui/modal"
+import {useForm} from "react-hook-form"
+import { zodResolver} from '@hookform/resolvers/zod'
+
+const formSchema = z.object({
+        name: z.string().min(1)
+})
 
 export const StoreModal = () => {
 const storeModal = useStoreModal();
+
+const form = useForm<z.infer<typeof formSchema>>({
+resolver: zodResolver(formSchema),
+defaultValues:{
+        name:""
+}
+})
 return(
 <Modal 
 title="create store"
@@ -12,7 +27,7 @@ description ="add a new store to manage products and categories"
 isOpen = {storeModal.isOpen}
 onClose={storeModal.onClose}>
         future content  table
-</Modal>
+</Modal> 
 )
 }
 
