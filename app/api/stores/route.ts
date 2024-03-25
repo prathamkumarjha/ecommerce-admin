173213888a0b6ef558391a1088840618f6a26fba
prathamkumarjha <prathamjha123dcs@gmail.com>
2024-03-25 13:36:27 +0530

    made some brutal changes

---
+++ app/api/stores/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+import {auth } from "@clerk/nextjs"; 
+
+import prismadb from "@/lib/prismadb";
+
+export async function POST ( req:Request, ) {
+
+    try{
+        const {userId} = await auth();
+        const body = await req.json();
+
+        const {name} = body;
+        
+        if(!userId){
+            return new NextResponse("not authenticated", {status:400})
+        }
+
+        if(!name){
+            return new NextResponse("Name is required" , {status:400})
+        }
+
+        console.log(name)
+
+        const store = await prismadb.store.create({
+            data:{
+                name: name,
+                userId
+            }
+        })
+
+        return NextResponse.json(store)
+    }
+    catch(error){
+            console.log("[stores/route",error)
+            return new NextResponse("Internal error", {status:500})
+    }
+
+}
