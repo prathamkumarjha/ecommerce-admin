8dbc1227590c58c0eaa63040f74823fb9e2a2f00
prathamkumarjha <prathamjha123dcs@gmail.com>
2024-03-25 17:32:07 +0530

    modal is working

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex justify-center items-center h-screen">{children}</div>
+  );
+};
+export default AuthLayout;
--- components/modals/store-modals.tsx
+++ components/modals/store-modals.tsx
@@ -1,15 +1,15 @@
-"use client"
+"use client";
 
-import { Modal } from "@/components/ui/modal"
-import {useStoreModal} from "@/hooks/useStoreModal"
- 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import {z} from  "zod"
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/useStoreModal";
 
-import axios from "axios"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-import { Button } from "@/components/ui/button"
+import axios from "axios";
+
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -17,65 +17,79 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 const formSchema = z.object({
-    name: z.string().min(1,{
+  name: z.string().min(1, {
     message: "name of the store is required",
-    }),
-  })
+  }),
+});
 
+export const StoreModal = () => {
+  const storeModal = useStoreModal();
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+  const [disabled, setDisabled] = useState(false);
 
-  export const StoreModal = () => {
-    const storeModal = useStoreModal()
-
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            name: "",
-        }
-    })
-
-    const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    try{
-      const response = await  axios.post('/api/stores',values)
-        console.log("done",response);
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setDisabled(true);
+      const response = await axios.post("/api/stores", values);
+      toast.success("store created");
+    } catch (error) {
+      console.log("error in submission", error);
+      toast.error("store creation failed");
+    } finally {
+      setDisabled(false);
     }
-    catch(error){
-        console.log("error in submission",error)
-    }
-    }
+  };
 
-    return (
-        <Modal
-            title="Store Name"
-            description="Give a name to your store"
-            onClose={storeModal.onClose}
-            isOpen={storeModal.isOpen}
-        >
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                    <FormField
-                        control={form.control}
-                        name="name"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Store Name</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="E-Commerce" {...field} />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <div className="flex justify-end space-x-4">
-                        <Button className="mt-1" variant="outline" onClick={storeModal.onClose}>Cancel</Button>
-                        <Button className="mt-1"  type="submit">Submit</Button>
-                    </div>
-                </form>
-            </Form>
-        </Modal>
-    )
-} 
+  return (
+    <Modal
+      title="Store Name"
+      description="Give a name to your store"
+      onClose={storeModal.onClose}
+      isOpen={storeModal.isOpen}
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Store Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="E-Commerce" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex justify-end space-x-4">
+            <Button
+              className="mt-1"
+              variant="outline"
+              disabled={disabled}
+              onClick={storeModal.onClose}
+              type="button"
+            >
+              Cancel
+            </Button>
+            <Button className="mt-1" disabled={disabled} type="submit">
+              Submit
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </Modal>
+  );
+};
